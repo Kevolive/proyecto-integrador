1afa0a07dcc8075d8ff19b8307a5fcd5fe6672df
Kevin Olivella <kevinolivella@gmail.com>
2025-03-31 22:52:31 -0500

    Se arregla el openpyxl

--- pages/1_📌_M2 Actvidad 1.py
+++ pages/1_📌_M2 Actvidad 1.py
@@ -157,7 +157,7 @@ st.code(code_archivo, language='python')
 
 ### Ejercicio 6: Crear DataFrame con un archivo Excel
 st.subheader("""6- DataFrame  con Excel""")
-df_excel = pd.read_excel("/data.xlsx", engine="openpyxl")
+df_excel = pd.read_excel("pages/data.xlsx", engine="openpyxl")
 
 st.dataframe(df_excel)
 st.write("### Código para crear el DataFrame:")
