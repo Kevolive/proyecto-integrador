cf98e012b954557ce2d590f1df241899bd4630b9
Kevin Olivella <kevinolivella@gmail.com>
2025-03-31 23:02:25 -0500

    Se arregla el openpyxl

--- pages/1_📌_M2 Actvidad 1.py
+++ pages/1_📌_M2 Actvidad 1.py
@@ -157,18 +157,18 @@ st.dataframe(df_archivo)
 st.code(code_archivo, language='python')
 
 ### Ejercicio 6: Crear DataFrame con un archivo Excel
-st.subheader("""6- DataFrame  con Excel""")
-df_excel =pd.read_excel("data.xlsx", engine='openpyxl')
+# st.subheader("""6- DataFrame  con Excel""")
+# df_excel =pd.read_excel("data.xlsx", engine='openpyxl')
 
-st.dataframe(df_excel)
-st.write("### Código para crear el DataFrame:")
-code_excel="""
-import streamlit as st
-import pandas as pd
-df_excel = pd.read_excel("data.xlsx")
-st.dataframe(df_excel)
-"""
-st.code(code_excel, language='python')
+# st.dataframe(df_excel)
+# st.write("### Código para crear el DataFrame:")
+# code_excel="""
+# import streamlit as st
+# import pandas as pd
+# df_excel = pd.read_excel("data.xlsx")
+# st.dataframe(df_excel)
+# """
+# st.code(code_excel, language='python')
 
 ### Ejercicio 7: Crear DataFrame con un archivo JSON
 st.subheader("""7- DataFrame  con JSON""")
