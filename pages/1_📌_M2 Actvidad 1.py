72883e19c78ee3c236a822d1dcf9e0321bb2b119
Kevin Olivella <kevinolivella@gmail.com>
2025-03-31 21:26:48 -0500

    Se realizan los ejercicios de listas de listas y de series

--- pages/1_📌_M2 Actvidad 1.py
+++ pages/1_📌_M2 Actvidad 1.py
@@ -28,7 +28,7 @@ st.markdown("""
 st.header("Solución")
 st.subheader("Actividad 1 - Creación de DataFrames de libros con diferentes fuentes", divider=True)
 
-### Creación de DataFrame de libros con diccionario
+### Ejercicio 1. Creación de DataFrame de libros con diccionario
 st.subheader("""1- DataFrame de libros con Diccionario:""")
 
 libros = {
@@ -58,7 +58,7 @@ st.write("DataFrame de libros:")
 st.dataframe(df_libros)"""
 st.code(code, language='python')
 
-### Creación de DataFrame de ciudades con lista de diccionarios
+### Ejercicio 2: Creación de DataFrame de ciudades con lista de diccionarios
 st.subheader("""2- DataFrame de ciudades con lista de diccionario:""")
 ciudades = [
     {"Ciudad": "Medellín", "País": "Colombia", "Población": 2500000},
@@ -84,3 +84,56 @@ df_ciudades = pd.DataFrame(ciudades)
 st.dataframe(df_ciudades)
 """
 st.code(code_ciudades, language='python')
+
+###Ejercicio 3: Listas de listas
+
+st.subheader("""3- DataFrame de productos con lista de listas""")
+productos = [
+    ["Laptop", 1200, 16],
+    ["Silla", 150, 25],
+    ["Libro", 20, 100],
+    ["Teléfono", 800, 8]
+]
+df_productos = pd.DataFrame(productos, columns=["Nombre", "Precio", "Cantidad en stock"])
+
+st.dataframe(df_productos)
+
+st.write("### Código para crear el DataFrame de productos:")
+code_productos="""" 
+import streamlit as st
+import pandas as pd
+
+
+"productos = [
+    ["Laptop", 1200, 16],
+    ["Silla", 150, 25],
+    ["Libro", 20, 100],
+    ["Teléfono", 800, 8]
+]
+df_productos = pd.DataFrame(productos, columns=["Nombre", "Precio", "Cantidad en stock"])
+st.DataFrame(df_productos) """
+st.code(code_productos, language='python')
+
+### Ejercicio 4: Crear dataFrame con series.
+st.subheader("""4- DataFrame de estudiantes con Series""")
+
+nombres = pd.Series(["Kevin", "Debora", "Marvin", "Paola"])
+edades = pd.Series([20, 22, 19, 21])
+ciuddad = pd.Series(["Medellín", "Cali", "Bogotá", "Cartagena"])
+
+df_nombres = pd.DataFrame({'Nombres': nombres, 'Edades': edades, 'Ciudad': ciuddad})
+st.dataframe(df_nombres)
+st.write("### Código para crear el DataFrame de estudiantes:")
+code_nombres=""" 
+"import streamlit as st
+import pandas as pd
+"st.subheader(4- DataFrame de estudiantes con Series)
+
+nombres = pd.Series(["Kevin", "Debora", "Marvin", "Paola"])
+edades = pd.Series([20, 22, 19, 21])
+ciuddad = pd.Series(["Medellín", "Cali", "Bogotá", "Cartagena"])
+
+df_nombres = pd.DataFrame({'Nombres': nombres, 'Edades': edades, 'Ciudad': ciuddad})
+st.dataframe(df_nombres)
+"""
+st.code(code_nombres, language='python')
